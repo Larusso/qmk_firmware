dd82a3ad44623da79f3014c37e80dfef24802bbb
skullY <skullydazed@gmail.com>
2019-10-12 09:47:25 -0700

    wip

--- keyboards/1upkeyboards/sweet16/rules.mk
+++ keyboards/1upkeyboards/sweet16/rules.mk
@@ -1,18 +1,4 @@
-# MCU name
-MCU = atmega32u4
-
-# Bootloader selection
-#   Teensy       halfkay
-#   Pro Micro    caterina
-#   Atmel DFU    atmel-dfu
-#   LUFA DFU     lufa-dfu
-#   QMK DFU      qmk-dfu
-#   ATmega32A    bootloadHID
-#   ATmega328P   USBasp
-BOOTLOADER = caterina
-
 # Build Options
-#   comment out to disable the options.
 #
 BOOTMAGIC_ENABLE = yes # Virtual DIP switch configuration(+1000)
 MOUSEKEY_ENABLE = yes  # Mouse keys(+4700)
@@ -24,4 +10,3 @@ NKRO_ENABLE = yes      # USB Nkey Rollover - if this doesn't work, see here: htt
 BACKLIGHT_ENABLE = no  # Enable keyboard backlight functionality
 AUDIO_ENABLE = no
 RGBLIGHT_ENABLE = yes
-EXTRAFLAGS += -flto
